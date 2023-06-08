04b3e9031c7c740105ca1891aa797add5a2d7e55
hirokidaichi <hirokidaichi@gmail.com>
2023-06-08 09:41:55 +0900

    Add diffloader.ts for Git diff management.

--- command.ts
+++ command.ts
@@ -1,9 +1,14 @@
 import { loadQARefineChain } from "langchain/chains";
-import { OpenAI } from "langchain/llms/openai";
-import { Command } from "https://deno.land/x/cliffy@v0.25.7/command/mod.ts";
-import { ConsoleCallbackHandler } from "langchain/callbacks";
-import { loadDiffFromFile, loadDiffFromGit } from "./diffloader.ts";
+import { ChatOpenAI } from "langchain/chat_models/openai";
 import { Document } from "langchain/document";
+import { Select } from "https://deno.land/x/cliffy@v0.25.7/prompt/select.ts";
+
+import { Command } from "https://deno.land/x/cliffy@v0.25.7/command/mod.ts";
+import Spinner from "https://deno.land/x/cli_spinners@v0.0.2/mod.ts";
+import { loadDiffFromFile, loadDiffFromGit } from "./diffloader.ts";
+import { $ } from "https://deno.land/x/zx_deno@1.2.2/mod.mjs";
+
+$.verbose = false;
 
 const { options, args } = await new Command()
   .name("git-aico")
@@ -13,26 +18,69 @@ const { options, args } = await new Command()
   .arguments("[commit]")
   .parse(Deno.args);
 
-const modelGPT4 = new OpenAI({
+console.log(options, args);
+
+const modelGPT4 = new ChatOpenAI({
   modelName: "gpt-4",
-  temperature: 0.7,
-  maxTokens: 1000,
-  maxRetries: 5,
+  temperature: 0.1,
+  maxTokens: 2500,
 });
 
 const chain = loadQARefineChain(modelGPT4);
+const prompt = Deno.readTextFileSync("./prompt.txt").toString();
 
-const generateCommitMessage = async (
-  diffDocuments: Document[],
-): Promise<string> => {
-  const response = await chain.call({
-    question:
-      "Please generate an appropriate commit message based on the context.",
-    input_documents: diffDocuments,
-  });
-  return response.output_text;
+const trimMessage = (message: string): string => {
+  return message.trim().replace(/^-+\s*/, "");
 };
 
-const diffDocuments = await loadDiffFromGit();
-const commitMessage = await generateCommitMessage(diffDocuments);
-console.log(commitMessage);
+const generateCommitMessages = async (
+  diffDocuments: Document[],
+): Promise<string[]> => {
+  const response = await chain.call({
+    question: prompt,
+    input_documents: diffDocuments,
+  });
+  return response.output_text.split("\n").map(trimMessage);
+};
+
+const main = async (options: any, args: any) => {
+  ///const spinner = Spinner.getInstance();
+
+  //spinner.start("Thinking ...");
+  const diffDocuments = await loadDiffFromGit();
+  const commitMessages = await generateCommitMessages(diffDocuments);
+  //spinner.stop();
+
+  const candidates = commitMessages.map((message) => ({
+    name: message,
+    value: message,
+  }));
+
+  const message: string = await Select.prompt({
+    message: "Choose a commit message",
+    options: candidates,
+  });
+  const action: string = await Select.prompt({
+    message: "",
+    options: [
+      { name: "git commit ", value: "commit" },
+      { name: "git commit with editor", value: "commit-editor" },
+      { name: "exit", value: "exit" },
+    ],
+  });
+  if (action === "exit") {
+    return Deno.exit(0);
+  }
+  if (action === "commit") {
+    await $`git commit -m "${message}"`;
+  }
+  if (action === "commit-editor") {
+    const tmpfile = await Deno.makeTempFile();
+    await Deno.writeTextFile(tmpfile, message);
+    await $`git commit -t ${tmpfile}`;
+    await Deno.remove(tmpfile);
+  }
+  console.log(message);
+};
+
+await main(options, args);
